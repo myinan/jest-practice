7e7d97a49c5de938139bb58bd2baefbee39a335c
myinan <m.yasir.inan@gmail.com>
2024-01-29 02:36:41 +0300

    Complete the first 2 function tests

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import "./style.css";
 
-console.log("Hello world");
-
-export default function capitalize(word) {
+// 1. A capitalize function that takes a string and returns it with the first character capitalized.
+function capitalize(word) {
   const capitalized = word.charAt(0).toUpperCase() + word.slice(1);
 
   return capitalized;
 }
+
+// 2.  reverseString function that takes a string and returns it reversed.
+function reverseString(str) {
+  return str.split("").reverse().join("");
+}
+
+export { capitalize, reverseString };
--- src/tests/index.test.js
+++ src/tests/index.test.js
@@ -1,5 +1,9 @@
-import capitalize from "../index";
+import { capitalize, reverseString } from "../index";
 
-test("if world is capitalized", () => {
+test("passed in world is capitalized", () => {
   expect(capitalize("test")).toMatch("Test");
+});
+
+test("string is correctly reversed", () => {
+  expect(reverseString("Home")).toMatch("emoH");
 });
