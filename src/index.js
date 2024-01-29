481b6a46674bd3dda268f0ff97745b153bc22efe
myinan <m.yasir.inan@gmail.com>
2024-01-30 02:28:21 +0300

    Implement caesarCipher func and it's test

--- src/index.js
+++ src/index.js
@@ -7,9 +7,55 @@ function capitalize(word) {
   return capitalized;
 }
 
-// 2.  reverseString function that takes a string and returns it reversed.
+// 2. reverseString function that takes a string and returns it reversed.
 function reverseString(str) {
   return str.split("").reverse().join("");
 }
 
-export { capitalize, reverseString };
+// 3. A calculator object that contains functions for the basic operations:
+// add, subtract, divide, and multiply.
+// Each of these functions should take two numbers and return the correct calculation.
+class Calculator {
+  static add(numF, numS) {
+    return numF + numS;
+  }
+
+  static subtract(numF, numS) {
+    return numF - numS;
+  }
+
+  static divide(numF, numS) {
+    return numF / numS;
+  }
+
+  static multiply(numF, numS) {
+    return numF * numS;
+  }
+}
+
+// 4. A caesarCipher function that takes a string and a shift factor,
+// and returns it with each character “shifted”.
+function caesarCipher(text, shiftF) {
+  const alphabetLowerCase = "abcdefghijklmnopqrstuvwxyz".split("");
+  const alphabetUpperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+  const shiftedText = [];
+  const textArr = text.split("");
+
+  textArr.forEach((char) => {
+    if (alphabetLowerCase.includes(char)) {
+      shiftedText.push(
+        alphabetLowerCase[(alphabetLowerCase.indexOf(char) + shiftF) % 26],
+      );
+    } else if (alphabetUpperCase.includes(char)) {
+      shiftedText.push(
+        alphabetUpperCase[(alphabetUpperCase.indexOf(char) + shiftF) % 26],
+      );
+    } else {
+      shiftedText.push(char);
+    }
+  });
+
+  return shiftedText.join("");
+}
+
+export { capitalize, reverseString, Calculator, caesarCipher };
--- src/tests/index.test.js
+++ src/tests/index.test.js
@@ -1,4 +1,4 @@
-import { capitalize, reverseString } from "../index";
+import { capitalize, reverseString, Calculator, caesarCipher } from "../index";
 
 test("passed in world is capitalized", () => {
   expect(capitalize("test")).toMatch("Test");
@@ -6,4 +6,24 @@ test("passed in world is capitalized", () => {
 
 test("string is correctly reversed", () => {
   expect(reverseString("Home")).toMatch("emoH");
+});
+
+test("Add", () => {
+  expect(Calculator.add(5, 7)).toBe(12);
+});
+
+test("Subtract", () => {
+  expect(Calculator.subtract(10, 4)).toBe(6);
+});
+
+test("Divide", () => {
+  expect(Calculator.divide(20, 5)).toBe(4);
+});
+
+test("Multiply", () => {
+  expect(Calculator.multiply(14, 3)).toBe(42);
+});
+
+test("Ciphered input text correctly", () => {
+  expect(caesarCipher("Hello, World!", 10)).toBe("Rovvy, Gybvn!");
 });
